fbd2537a02da4ac1b9dd3c4ad277aff4fd1b7968
Michael Jo <michael.jo@gmail.com>
2019-08-23 16:38:39 -0700

    layout initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Icon from '@material-ui/core/Icon';
+import { Root, Header, Nav, Content, Footer, presets } from 'mui-layout';
+
+import NavContentEx from './components/NavContentEx';
+import NavHeaderEx from './components/NavHeaderEx';
+import HeaderEx from './components/HeaderEx';
+import ContentEx from './components/ContentEx';
+import FooterEx from './components/FooterEx';
+
+import './styles.css';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [preset, setPreset] = useState('createStandardLayout');
+  const [data, setData] = useState({
+    header: true,
+    nav: true,
+    content: true,
+    footer: true
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <MuiThemeProvider theme={createMuiTheme()}>
+      <Root
+        config={presets[preset]({ headerPosition: 'relative' })}
+        style={{ minHeight: '100vh' }}
+      >
+        <CssBaseline />
+        <Header
+          renderMenuIcon={opened =>
+            opened ? <Icon>chevron_left</Icon> : <Icon>menu_rounded</Icon>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {({ screen, collapsed }) =>
+            data.header && <HeaderEx screen={screen} collapsed={collapsed} />
+          }
+        </Header>
+        <Nav
+          renderIcon={collapsed =>
+            collapsed ? <Icon>chevron_right</Icon> : <Icon>chevron_left</Icon>
+          }
+          header={({ collapsed }) =>
+            data.nav && <NavHeaderEx collapsed={collapsed} />
+          }
+        >
+          {data.nav && <NavContentEx />}
+        </Nav>
+        <Content>{data.content && <ContentEx />}</Content>
+        <Footer>{data.footer && <FooterEx />}</Footer>
+      </Root>
+      )
+    </MuiThemeProvider>
   );
 }
 
